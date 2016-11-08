141f328cd45fc724e6977c92d835cdc702a5b524
guolunhao <guolunhao@gmail.com>
2016-11-08 13:48:13 +0800

    using xor to solve the #389

--- LeetCodeCpp/389.Find the Difference/Solution.h
+++ LeetCodeCpp/389.Find the Difference/Solution.h
@@ -5,29 +5,18 @@ using namespace std;
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int letterCountS[26] = { 0 };
-        int letterCountT[26] = { 0 };
+        if (t.length() - s.length() != 1)
+        {
+            return ' ';
+        }
+
+        int result = 0;
 
         for (size_t i = 0; i < s.length(); i++)
         {
-            char letter = s[i];
-            letterCountS[letter - 'a']++;
+            result ^= s[i] ^ t[i];
         }
 
-        for (size_t i = 0; i < t.length(); i++)
-        {
-            char letter = t[i];
-            letterCountT[letter - 'a']++;
-        }
-
-        for (size_t i = 0; i < 26; i++)
-        {
-            if (letterCountS[i] != letterCountT[i]) {
-                return i + 'a';
-            }
-        }
-
-        return ' ';
-
+        return result ^ t[t.length()-1];
     }
 };
