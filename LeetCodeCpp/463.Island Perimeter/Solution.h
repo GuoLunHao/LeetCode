0c947f5ba506f8960e21978be7967a24d1ecc58b
guolunhao <guolunhao@gmail.com>
2016-11-21 10:19:27 +0800

    solve the problem #463

---
+++ LeetCodeCpp/463.Island Perimeter/Solution.h
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int perimeter = 0;
+
+        for (size_t i = 0; i < grid.size(); i++)
+        {
+            for (size_t j = 0; j < grid[i].size(); j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    if (i == 0 || grid[i-1][j] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (j == 0 || grid[i][j-1] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (i == grid.size() - 1 || grid[i+1][j] == 0)
+                    {
+                        perimeter++;
+                    }
+                    if (j == grid[i].size() - 1 || grid[i][j+1] == 0)
+                    {
+                        perimeter++;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
